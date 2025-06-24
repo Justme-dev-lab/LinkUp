74427b9adb58160221ec208421ede1693e3a1253
MynamesMatcha <rafi.haritsya@gmail.com>
2025-06-24 14:57:12 +0700

    .....

--- app/src/main/java/com/example/linkup/MessageChatActivity.kt
+++ app/src/main/java/com/example/linkup/MessageChatActivity.kt
@@ -1,20 +1,32 @@
 package com.example.linkup
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MessageChatActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_message_chat)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+
+        send_message_btn.setOnClickListener {
+            var message: String = text_message.text.toString()
+            if (message == null)
+            {
+                Toast.makeText(this@MessageChatActivity,"Please write message", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                sendmessageToUser(firebaseUser!!.uid, userId, message)
+            }
         }
     }
 }
